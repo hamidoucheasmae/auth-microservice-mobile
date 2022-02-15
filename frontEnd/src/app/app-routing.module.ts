1516ba5fb936f66c1898675670f269be32305291
hamidoucheasmae <76485278+hamidoucheasmae@users.noreply.github.com>
2022-02-15 13:49:20 +0100

    update

--- frontEnd/src/app/app-routing.module.ts
+++ frontEnd/src/app/app-routing.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuardService } from './services/auth-guard.service';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'inside',
+    loadChildren: () => import('./pages/inside/inside.module').then( m => m.InsidePageModule),
+    canActivate: [AuthGuardService]
   },
 ];
 
---
+++ frontEnd/src/app/services/auth.service.ts
@@ -0,0 +1,199 @@
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+83
+84
+85
+86
+87
+88
+89
+90
+91
+92
+93
+94
+95
+96
+97
+98
+99
+import { Platform, AlertController } from '@ionic/angular';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Storage } from '@ionic/storage';
+import { environment } from '../../environments/environment';
+import { tap, catchError } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
+ 
+const TOKEN_KEY = 'access_token';
+ 
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+ 
+  url = environment.url;
+  user = null;
+  authenticationState = new BehaviorSubject(false);
+ 
+  constructor(private http: HttpClient, private helper: JwtHelperService, private storage: Storage,
+    private plt: Platform, private alertController: AlertController) {
+    this.plt.ready().then(() => {
+      this.checkToken();
+    });
+  }
+ 
+  checkToken() {
+    this.storage.get(TOKEN_KEY).then(token => {
+      if (token) {
+        let decoded = this.helper.decodeToken(token);
+        let isExpired = this.helper.isTokenExpired(token);
+ 
+        if (!isExpired) {
+          this.user = decoded;
+          this.authenticationState.next(true);
+        } else {
+          this.storage.remove(TOKEN_KEY);
+        }
+      }
+    });
+  }
+ 
+  register(credentials) {
+    return this.http.post(`${this.url}/api/register`, credentials).pipe(
+      catchError(e => {
+        this.showAlert(e.error.msg);
+        throw new Error(e);
+      })
+    );
+  }
+ 
+  login(credentials) {
+    return this.http.post(`${this.url}/api/login`, credentials)
+      .pipe(
+        tap(res => {
+          this.storage.set(TOKEN_KEY, res['token']);
+          this.user = this.helper.decodeToken(res['token']);
+          this.authenticationState.next(true);
+        }),
+        catchError(e => {
+          this.showAlert(e.error.msg);
+          throw new Error(e);
+        })
+      );
+  }
+ 
+  logout() {
+    this.storage.remove(TOKEN_KEY).then(() => {
+      this.authenticationState.next(false);
+    });
+  }
+ 
+  getSpecialData() {
+    return this.http.get(`${this.url}/api/special`).pipe(
+      catchError(e => {
+        let status = e.status;
+        if (status === 401) {
+          this.showAlert('You are not authorized for this!');
+          this.logout();
+        }
+        throw new Error(e);
+      })
+    )
+  }
+ 
+  isAuthenticated() {
+    return this.authenticationState.value;
+  }
+ 
+  showAlert(msg) {
+    let alert = this.alertController.create({
+      message: msg,
+      header: 'Error',
+      buttons: ['OK']
+    });
+    alert.then(alert => alert.present());
+  }
+}
